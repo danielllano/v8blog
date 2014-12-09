56cbfb5fce48608f4bcf520ba5ae86896e83a2a5
Daniel Llano <daniel.llano.a@gmail.com>
2014-12-09 14:44:47 -0500

    Added validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
   belongs_to :author
+
+  validates :title, presence: true,
+										length: {minimum: 3}
+
+	validates :content, presence: true,
+											length: {minimum: 10}
+
+	validates :published_date, presence: true
+
 end
